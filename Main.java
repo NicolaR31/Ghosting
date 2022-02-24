59762a6af7f9415d1f24c8d885f9694c40cb66a9
Damiano1234 <100383121+Damiano1234@users.noreply.github.com>
2022-02-24 22:14:44 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,95 +1,86 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        try (BufferedReader reader=new BufferedReader(new FileReader("Input.txt"));
-             BufferedWriter writer=new BufferedWriter(new FileWriter("Output.txt"))) {
-            String assigment;
-            String[] assigment2;
-            int R;
-            int C;
-            int N;
-            List<Integer> keys=new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader lettore = new BufferedReader(new FileReader("input.txt"));
+        BufferedWriter scrittore = new BufferedWriter(new FileWriter("output.txt"));
+        int tc = Integer.parseInt(lettore.readLine());
 
-            int temp=0;
+        generale:
+        for(int i = 0; i < tc; i++) {
+            String riga[] = lettore.readLine().split(" ");
+            int righe = Integer.parseInt(riga[0]);
+            int colonne = Integer.parseInt(riga[1]);
 
-            while(reader.ready()) {
-                keys.clear();
-                assigment= reader.readLine();
-                assigment2=assigment.split(" ");
-                R=Integer.parseInt(assigment2[0]);
-                try {
-                    C=Integer.parseInt(assigment2[1]);
-                    N=Integer.parseInt(assigment2[2]);
-                } catch (IndexOutOfBoundsException | NegativeArraySizeException e) {
-                    e.printStackTrace();
-                    temp++;
+            Integer [][] tastiera = new Integer[righe][colonne];
+            int assegnazione = 0;
+            for(int j = 0; j < tastiera.length; j++) {
+                for(int z = 0; z < tastiera[j].length; z++) {
+                    tastiera[j][z] = assegnazione;
+                    assegnazione++;
+                }
+            }
+
+            int nTasti = Integer.parseInt(riga[2]);
+            int posTastoGhosting = 0;
+
+            for(int a = 3; a < 3 + nTasti; a++) {
+
+                int tasto = Integer.parseInt(riga[a]);
+                Integer[] pos = cercaNumero(tastiera, tasto);
+                if(controlloGhosting(tastiera,tasto,pos[0], pos[1])){
+                    scrittore.write("Case #" + (i+1) + ": " + posTastoGhosting + "\n");
+                    continue generale;
+                }
+                posTastoGhosting++;
+            }
+            scrittore.write("Case #" + (i+1) + ": -1" + "\n");
+
+
+        }
+        scrittore.flush();
+    }
+
+    public static Integer[] cercaNumero(Integer [][] tastiera, int n) {
+        for(int j = 0; j < tastiera.length; j++) {
+            for(int z = 0; z < tastiera[j].length; z++) {
+                if(tastiera[j][z] == n){
+                    tastiera[j][z] = -1;
+                    return new Integer[]{j,z};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean controlloGhosting(Integer [][] tastiera, int n, int rigaN, int colonnaN) {
+        for(int j = 0; j < tastiera.length; j++) {
+
+            if(j == rigaN) {
+                continue;
+            }
+
+            for(int z = 0; z < tastiera[j].length; z++) {
+
+                if(z == colonnaN) {
                     continue;
                 }
-                Arrays.stream(assigment2).skip(3).mapToInt(Integer::parseInt).forEach(keys::add);
 
-                boolean[][] booleans=mapKeyboard(R, C, keys);
-/*
-                for(int i = 0; i<R; i++)
-                {
-                    for(int j = 0; j<C; j++)
-                    {
-                        System.out.print(booleans[i][j]);
-                    }
-                    System.out.println();
+                if(tastiera[j][z] == -1) {
+                    return true;
                 }
-*/
-                int result=scan(booleans, R, C);
-                System.out.println(result);
-                int index=keys.indexOf(result);
-                writer.write("Case #"+temp+": "+index+"\n");
-                temp++;
-
-            }
-            writer.flush();
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static int scan(boolean [][] keyboard, int R, int C) { //returns -1 if no pattern is find
-        
-    }
-
-    public static boolean[][] mapKeyboard(int R, int C, List<Integer> keys) {
-        boolean[][] g=new boolean[R][C];
-        int lastNumber=0;
-        for(int i=0; i<R; i++) {
-            for(int k=0; k<C; k++) {
-                if(keys.contains(lastNumber)) {
-                    g[i][k]=true;
-                }
-                else {
-                    g[i][k]=false;
-                }
-                lastNumber++;
             }
         }
-        return g;
-    }
-
-    public static int convertToKey(int i, int k, int R, int C) {
-        int lastNumber=0;
-        for(int f=0; f<R; f++) {
-            for(int g=0; g<C; g++) {
-                if(f==i && g==k) {
-                    return lastNumber;
-                }
-                lastNumber++;
-            }
-        }
-        return -1;
+        return false;
     }
 }
 
+
+/*
+            for(int j = 0; j < tastiera.length; j++) {
+                for(int z = 0; z < tastiera[j].length; z++) {
+                    System.out.print(tastiera[j][z] + "\t");
+                }
+                System.out.println("\n");
+            }
+*/
